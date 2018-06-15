b378fa995c318838cf33d8cb39dacd8b25e4ce1b
PC-NANAILLE\Rigal <anais.rigal29@hotmail.fr>
2018-06-15 11:36:08 +0200

    Affichage des stats

--- src/main/resources/static/js/statistiquesController.js
+++ src/main/resources/static/js/statistiquesController.js
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 
-app.controller("statistiquesController", function() {
-    
+app.controller("statistiquesController", function($scope,$http) {
+    $http.get("http://localhost:8080/api/membre/statistiques")
+    .then(function(response) {
+        $scope.content = response.data;
+        $scope.statuscode = response.status;
+        $scope.statustext = response.statusText; 
+    });
 });
