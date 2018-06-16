e0f44fc233cabe98f49e5ec5f931e46e8499205b
PC-NANAILLE\Rigal <anais.rigal29@hotmail.fr>
2018-06-16 18:03:10 +0200

    v4 afficher les cours d'un membre

--- src/main/resources/static/js/consultationController.js
+++ src/main/resources/static/js/consultationController.js
@@ -4,15 +4,11 @@
  * and open the template in the editor.
  */
 app.controller("consultationController", function($scope,$http,$window,$cookies,$location) {
-if ($cookies.get("type")!="Membre"){
-      $window.alert("Vous n'avez pas les droits :(")
-     $location.path("/piscines");
- }
-    $http.get("http://localhost:8081/api/cours/afficher/"/*RECUPERATION DE L'ID DU MEMBRE CONNECTE*/)
+
+    $http.get("http://localhost:8081/api/cours/afficher/"+$cookies.get("id"))
     .then(function(response) {
-        $scope.content = response.data;
-        $scope.statuscode = response.status;
-        $scope.statustext = response.statusText; 
+        $scope.membre = response.data;
+		
     });
     
 });
