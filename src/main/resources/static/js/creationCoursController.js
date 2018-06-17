f9fe87de3336eb10e09d0c1547d9fa5b2d6f8d98
PC-NANAILLE\Rigal <anais.rigal29@hotmail.fr>
2018-06-17 14:44:29 +0200

    v4 suite création créer cours

--- src/main/resources/static/js/creationCoursController.js
+++ src/main/resources/static/js/creationCoursController.js
@@ -3,5 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+app.controller("creationCoursController", function($scope, $http,$window,$cookies,$location) {
+    
+if ($cookies.get("type")!="Enseignant"){
+      $window.alert("Vous n'avez pas les droits :(")
+     $location.path("/piscines");
+ }
+ $http.get("http://localhost:8081/api/cours/afficherPiscines")
+    .then(function(response) {
+        $scope.piscine = response.data;
+        $scope.statuscode = response.status;
+        $scope.statustext = response.statusText; 
+    });
 
+});
 
